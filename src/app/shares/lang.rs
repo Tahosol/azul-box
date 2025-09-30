94d7038920b24e53f661651b95c43dc4cc4188cd
musdx <blue.neutron460@slmail.me>
2025-09-30 14:41:04 -0400

    added more lang

--- src/app/shares/lang.rs
+++ src/app/shares/lang.rs
@@ -7,43 +7,87 @@ pub struct LangThing {}
 impl LangThing {
     pub fn lang_chooser(ui: &mut Ui, mut lang_in: String) -> String {
         let language_codes: Vec<&str> = vec![
-            "en", // English
-            "fr", // French
-            "es", // Spanish
-            "zh", // Chinese
-            "de", // German
-            "ja", // Japanese
-            "ar", // Arabic
-            "ru", // Russian
-            "it", // Italian
-            "pt", // Portuguese
-            "nl", // Dutch
-            "sv", // Swedish
-            "no", // Norwegian
-            "fi", // Finnish
-            "da", // Danish
-            "pl", // Polish
-            "cs", // Czech
-            "hu", // Hungarian
-            "ro", // Romanian
-            "tr", // Turkish
-            "vi", // Vietnamese
-            "ko", // Korean
-            "el", // Greek
-            "he", // Hebrew
-            "th", // Thai
-            "id", // Indonesian
-            "ms", // Malay
-            "hi", // Hindi
-            "uk", // Ukrainian
-            "bg", // Bulgarian
-            "hr", // Croatian
-            "sk", // Slovak
-            "sl", // Slovenian
-            "sr", // Serbian
-            "lt", // Lithuanian
-            "lv", // Latvian
-            "et", // Estonian
+            "en",  // English
+            "fr",  // French
+            "es",  // Spanish
+            "zh",  // Chinese
+            "de",  // German
+            "ja",  // Japanese
+            "ar",  // Arabic
+            "ru",  // Russian
+            "it",  // Italian
+            "pt",  // Portuguese
+            "nl",  // Dutch
+            "sv",  // Swedish
+            "no",  // Norwegian
+            "fi",  // Finnish
+            "da",  // Danish
+            "pl",  // Polish
+            "cs",  // Czech
+            "hu",  // Hungarian
+            "ro",  // Romanian
+            "tr",  // Turkish
+            "vi",  // Vietnamese
+            "ko",  // Korean
+            "el",  // Greek
+            "he",  // Hebrew
+            "th",  // Thai
+            "id",  // Indonesian
+            "ms",  // Malay
+            "hi",  // Hindi
+            "uk",  // Ukrainian
+            "bg",  // Bulgarian
+            "hr",  // Croatian
+            "sk",  // Slovak
+            "sl",  // Slovenian
+            "sr",  // Serbian
+            "lt",  // Lithuanian
+            "lv",  // Latvian
+            "et",  // Estonian
+            "fa",  // Persian
+            "sw",  // Swahili
+            "ta",  // Tamil
+            "bn",  // Bengali
+            "ur",  // Urdu
+            "gu",  // Gujarati
+            "pa",  // Punjabi
+            "te",  // Telugu
+            "ml",  // Malayalam
+            "kn",  // Kannada
+            "mr",  // Marathi
+            "eu",  // Basque
+            "gl",  // Galician
+            "ca",  // Catalan
+            "is",  // Icelandic
+            "mt",  // Maltese
+            "sq",  // Albanian
+            "mk",  // Macedonian
+            "az",  // Azerbaijani
+            "hy",  // Armenian
+            "ka",  // Georgian
+            "af",  // Afrikaans
+            "zu",  // Zulu
+            "xh",  // Xhosa
+            "yo",  // Yoruba
+            "ig",  // Igbo
+            "am",  // Amharic
+            "my",  // Burmese
+            "km",  // Khmer
+            "lo",  // Lao
+            "mn",  // Mongolian
+            "si",  // Sinhala
+            "ne",  // Nepali
+            "jw",  // Javanese
+            "su",  // Sundanese
+            "fil", // Filipino
+            "tl",  // Tagalog
+            "ceb", // Cebuano
+            "haw", // Hawaiian
+            "sm",  // Samoan
+            "fj",  // Fijian
+            "to",  // Tongan
+            "ht",  // Haitian Creole
+            "la",  // Latin
         ];
         let languages: Vec<&str> = vec![
             "English",
@@ -83,6 +127,50 @@ impl LangThing {
             "Lithuanian",
             "Latvian",
             "Estonian",
+            "Persian",
+            "Swahili",
+            "Tamil",
+            "Bengali",
+            "Urdu",
+            "Gujarati",
+            "Punjabi",
+            "Telugu",
+            "Malayalam",
+            "Kannada",
+            "Marathi",
+            "Basque",
+            "Galician",
+            "Catalan",
+            "Icelandic",
+            "Maltese",
+            "Albanian",
+            "Macedonian",
+            "Azerbaijani",
+            "Armenian",
+            "Georgian",
+            "Afrikaans",
+            "Zulu",
+            "Xhosa",
+            "Yoruba",
+            "Igbo",
+            "Amharic",
+            "Burmese",
+            "Khmer",
+            "Lao",
+            "Mongolian",
+            "Sinhala",
+            "Nepali",
+            "Javanese",
+            "Sundanese",
+            "Filipino",
+            "Tagalog",
+            "Cebuano",
+            "Hawaiian",
+            "Samoan",
+            "Fijian",
+            "Tongan",
+            "Haitian Creole",
+            "Latin",
         ];
         ui.menu_button("Languages", |ui| {
             egui::ScrollArea::vertical()
