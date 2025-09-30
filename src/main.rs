7854133506da538435577fed8f780207e4f92d89
musdx <blue.neutron460@slmail.me>
2025-09-30 18:30:26 -0400

    cleaner ui

--- src/main.rs
+++ src/main.rs
@@ -88,16 +88,22 @@ impl eframe::App for MainApp {
         egui::CentralPanel::default().show(ctx, |ui| ui.label(""));
         egui::SidePanel::left("Panel")
             .resizable(true)
-            .width_range(27.0..=90.0)
+            .width_range(50.0..=100.0)
             .show(ctx, |ui| {
-                ui.label(RichText::new("Yt-dlp:").size(17.0));
-                ui.add(egui::Checkbox::without_text(&mut self.yt));
+                ui.add(egui::Checkbox::new(
+                    &mut self.yt,
+                    egui::RichText::new("YT-Dl").size(17.0),
+                ));
                 ui.separator();
-                ui.label(RichText::new("Pinterest:").size(17.0));
-                ui.add(egui::Checkbox::without_text(&mut self.pin));
+                ui.add(egui::Checkbox::new(
+                    &mut self.pin,
+                    egui::RichText::new("Pin-Dl").size(17.0),
+                ));
                 ui.separator();
-                ui.label(RichText::new("Ffmpeg:").size(17.0));
-                ui.add(egui::Checkbox::without_text(&mut self.ffmpeg));
+                ui.add(egui::Checkbox::new(
+                    &mut self.ffmpeg,
+                    egui::RichText::new("FFmpeg").size(17.0),
+                ));
                 ui.separator();
             });
         if self.yt {
