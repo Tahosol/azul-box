792f7023a472b732228b144fa44b695b52de7755
musdx <blue.neutron460@slmail.me>
2026-01-04 16:49:54 -0500

    refactor: move translate

--- src/app/cores/lrclib.rs
+++ src/app/cores/lrclib.rs
@@ -1,10 +1,11 @@
 use std::error::Error;
 use std::path::Path;
+use url::form_urlencoded;
 
 use lofty::{self, config::WriteOptions, prelude::*, probe::Probe, tag::Tag};
 use serde::Deserialize;
 
-use crate::app::cores::string_cleaner;
+use crate::app::cores::{string_cleaner, translate::translate};
 
 pub fn lrclib_fetch(opt: &Path, lang: &str) -> Result<(), Box<dyn Error>> {
     let mut tagged_file = Probe::open(&opt)?.read()?;
@@ -70,33 +71,4 @@ fn fetch(query: &str) -> Result<String, Box<dyn Error>> {
     } else {
         Ok(String::new())
     }
-}
-
-use serde_json::Value;
-use url::form_urlencoded;
-
-fn translate(to: &str, text: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let en_text: String = form_urlencoded::byte_serialize(text.as_bytes()).collect();
-    let url = format!(
-        "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl={}&dt=t&q={}",
-        to, en_text
-    );
-
-    let mut translated_text = text.to_string();
-
-    let json_as_string = ureq::get(&url).call()?.body_mut().read_to_string()?;
-    let values = serde_json::from_str::<Value>(&json_as_string)?;
-    if let Some(value) = values.get(0) {
-        println!("{:?}", value.as_str());
-        if let Some(list) = value.as_array() {
-            let lyrics: Vec<String> = list
-                .iter()
-                .filter_map(|v| v.get(0).and_then(|v| v.as_str()))
-                .map(|s| s.to_string())
-                .collect();
-            translated_text = lyrics.join("");
-            println!("Translate success!");
-        }
-    }
-    Ok(translated_text)
 }
